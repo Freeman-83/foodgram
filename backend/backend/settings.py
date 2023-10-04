0d82adfca9e7cdb2dc2650365b0e836b1ba3e6bb
Михаил Марин <mmn-83@yandex.ru>
2023-10-04 20:33:32 +0300

    Ready to rev. 2.0

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -5,9 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='key')
 
-# DEBUG = os.getenv('DEBUG', default=False)
-
-DEBUG = True
+DEBUG = os.getenv('DEBUG', default=False)
 
 ALLOWED_HOSTS = ['51.250.101.169', '127.0.0.1', 'localhost', 'foodmaster.ddns.net']
 
